a5999dc615041785b88f8327579274b76c1f905c
jeffthemaximum <frey.maxim@gmail.com>
2015-11-25 11:26:02 -0500

    settings for django

--- apis/wsgi.py
+++ apis/wsgi.py
@@ -1,16 +1,4 @@
-"""
-WSGI config for apis project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
-import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apis.settings")
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
