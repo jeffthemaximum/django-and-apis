c31732ca6be98300d96a797e9f0d5a0f9343e2b1
jeffthemaximum <frey.maxim@gmail.com>
2015-11-25 11:56:09 -0500

    wsgi file corrected for heroku

--- apis/wsgi.py
+++ apis/wsgi.py
@@ -1,3 +1,9 @@
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apis.settings")
+
 from whitenoise.django import DjangoWhiteNoise
 
-application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(get_wsgi_application())
