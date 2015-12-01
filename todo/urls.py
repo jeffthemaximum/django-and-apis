3174114e86fe5fd044a35277bec8c8eb38b88632
jeffthemaximum <frey.maxim@gmail.com>
2015-12-01 13:20:06 -0500

    views and urls in todo allow users to go to restful url

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^(?P<username>\w+)/$', views.user_todo, name='user_todo'),
     url(r'^$', views.todo_index, name="todo_index"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 
 
 def todo_index(request):
-    return render(request, 'todo/todo_index.html', {})
+    # if user is logged in
+    if request.user.is_authenticated():
+        # get username
+        username = request.user.username
+        # TODO
+        return redirect('todo.views.user_todo', username=username)
+    # if user not logged in, show template that tells them to sign up
+    else:
+        return render(request, 'todo/todo_index.html', {})
+
+
+def user_todo(request, username):
+    return render(request, 'todo/user_todo.html', {'username': username})
