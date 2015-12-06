ec29a279a7ee0f055a33812e384a90ea73cc2a21
jeffthemaximum <frey.maxim@gmail.com>
2015-12-06 13:51:37 -0500

    js for tasks accepts tasks and displays on template

--- todo/templates/todo/js/todo_scripts.js
+++ todo/templates/todo/js/todo_scripts.js
@@ -1,3 +1,4 @@
+tasks = [];
 
 
 $(document).ready(function() {
@@ -8,9 +9,25 @@ $(document).ready(function() {
         console.log("button clicked!");
         console.log(taskDescription);
         // reject if blank
-        // else save the value to some data structure
-        // clear the contents of test-description text box
-        // add html div with task description
+        if (taskDescription == "") {
+            console.log("it's blank!");
+            $("div#task.input-group").addClass("input-group has-error");
+        } else {
+            //unhide hidden label
+            $("div.hidden").addClass("unhidden");
+            $("div.hidden").removeClass("hidden");
+            // undo error input box
+            $("div#task").removeClass("input-group has-error");
+            $("div#task").addClass("input-group");
+            // else save the value to some data structure
+            tasks.push(taskDescription);
+            console.log(tasks);
+            // clear the contents of test-description text box
+            $("#task-description").val("");
+            // add html div with task description
+            $('ul.list-group').append('<li class="list-group-item">' + taskDescription + '</li>');
+        }
+        
         // send all tasks to django when form submitted
     });
 
