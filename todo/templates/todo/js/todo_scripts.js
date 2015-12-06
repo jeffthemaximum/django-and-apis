af6ca102087534bcbafee595c6ba39069cabee57
jeffthemaximum <frey.maxim@gmail.com>
2015-12-06 11:11:29 -0500

    pseudocode for js

--- todo/templates/todo/js/todo_scripts.js
+++ todo/templates/todo/js/todo_scripts.js
@@ -1,9 +1,17 @@
+
+
 $(document).ready(function() {
 
     // get task button when clicked!
     $("#task-button").click(function(event){
+        var taskDescription = $("#task-description").val();
         console.log("button clicked!");
-        console.log($("#task-description").val());
+        console.log(taskDescription);
+        // reject if blank
+        // else save the value to some data structure
+        // clear the contents of test-description text box
+        // add html div with task description
+        // send all tasks to django when form submitted
     });
 
 });
