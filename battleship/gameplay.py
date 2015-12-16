3a8ce3516b077ba036f013d82655f8c7a0d5cc2a
jeffthemaximum <frey.maxim@gmail.com>
2015-12-16 16:04:45 -0500

    board prints resonably well

--- battleship/gameplay.py
+++ battleship/gameplay.py
@@ -1,3 +1,19 @@
+import sys
+
+
+class Color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
 class Board:
     def __init__(self):
         self.ships = {
@@ -7,7 +23,6 @@ class Board:
             'cruiser': Ship(3),
             'destroyer': Ship(2)
         }
-        self.boardstate_fen = ""
 
     def initiate_boardstate_fen(self):
         ''' initiates board to string. row is 10 zeros. 10 columns of these rows '''
@@ -15,13 +30,25 @@ class Board:
         boardstate_fen = "/".join([boardstate_row for i in range(10)])
         self.boardstate_fen = boardstate_fen
 
+    def print_board(self):
+        # print first row
+        sys.stdout.write("  ")
+        for i in range(ord('A'), ord('K')):
+            sys.stdout.write(Color.UNDERLINE + chr(i) + Color.END)
+        print ""
+
+        #print rows
+        for i in range(10):
+            sys.stdout.write(str(i))
+            sys.stdout.write("|")
+            for i in range(11):
+                if i != 10:
+                    sys.stdout.write(self.boardstate_fen[i])
+            print ""
 
 class Ship:
     def __init__(self, length):
         self.length = length
-        self.start_row = ""
-        self.start_column = ""
-        self.orientation = ""
 
     def initialize_ship(self, start_row, start_column, orientation):
         self.start_row = start_row
@@ -61,9 +88,9 @@ class Game:
         for player in self.players:
             if player.player_type == 'Human':
                 for ship_name, ship_object in player.board.ships.iteritems():
-                    start_row = raw_input("What row do you want to put your " + ship_name + " in?")
-                    start_column = raw_input("What column do you want to put your " + ship_name + " in?")
-                    orientation = raw_input("What direction do you want your " + ship_name + " to point?")
+                    start_row = raw_input("What row do you want to put your " + ship_name + " in? ")
+                    start_column = raw_input("What column do you want to put your " + ship_name + " in? ")
+                    orientation = raw_input("What direction do you want your " + ship_name + " to point? ")
                     ship_object.initialize_ship(start_row=start_row, start_column=start_column, orientation=orientation)
             else:
                 # computer players
@@ -77,6 +104,8 @@ player2 = HumanPlayer()
 # game needs an array of players
 game = Game([player1, player2])
 
-print player1.board.boardstate_fen
+# setup and print board for player1
+player1.board.initiate_boardstate_fen()
+player1.board.print_board()
 
 game.setup_ships()
